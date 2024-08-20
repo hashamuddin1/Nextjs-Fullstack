d1e4e0b832fcd950125182dcaa0bc2e3b52cfba1
hashamuddin1 <hashamlaptop@gmail.com>
2024-08-20 23:52:28 +0500

    next response

--- next-api/src/app/api/user/route.js
+++ next-api/src/app/api/user/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request) {
-  return new Response("Hello Users");
+  return NextResponse.json({ firstName: "Hasham", lastName: "Uddin" });
 }
