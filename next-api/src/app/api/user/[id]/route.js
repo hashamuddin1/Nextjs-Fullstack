2d35e50960ac0d252cb4b07ff1ea5b3081ffd4c4
hashamuddin1 <hashamlaptop@gmail.com>
2024-08-28 22:07:13 +0500

    create dummy database

--- next-api/src/app/api/user/[id]/route.js
+++ next-api/src/app/api/user/[id]/route.js
@@ -1,7 +1,8 @@
 import { user } from "@/util/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function GET(request,content) {
+  console.log(content.params.id)
   const data = user;
   return NextResponse.json(data, { status: 200 });
 }
---
+++ next-api/src/util/db.js
@@ -0,0 +1,12 @@
+export const user = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    email: "jane@gmail.com",
+  },
+];
